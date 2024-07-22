4ac5b154668da5808ea797ee90f1aedbed74b912
Fauziah Syifa <fauziah13siregar@gmail.com>
2024-07-22 11:55:28 +0700

    "Deleted adminlte_footer.php, adminlte_header.php, footer.php, header.php, and sidebar.php files from app/Views/layouts directory."

--- app/Views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-    <footer class="main-footer">
-        <p>&copy; 2024 Pertamina EP Cepu. All rights reserved.</p>
-    </footer>
-    <link rel="icon" href="/img/logobungben.png" type="image/png">
-</body>
-</html>
